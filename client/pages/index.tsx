b9cd488fa2307631af63f2a653045a45705a4b54
KE5HBB <114964721+KE5HBB@users.noreply.github.com>
2020-03-12 09:27:39 +0000

    Indentation

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,4 +2,10 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { Button } from "@mantine/core";
-import Particles from "r
+import Particles from "react-tsparticles";
+import Head from "next/head";
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>
