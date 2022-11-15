9846e7cfcfd75dd7be168fe7bdc98996bcd5893e
KE5HBB <114964721+KE5HBB@users.noreply.github.com>
2022-11-15 17:50:27 +0000

    JsRender/NodeToGlade.vala

--- night_protocol/src/blockchain/master_chain.rs
+++ night_protocol/src/blockchain/master_chain.rs
@@ -1,4 +1,11 @@
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
-use super::block
+use super::blockchain::*;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Master {
+    pub master_blocks: Vec<MasterBlock>,
+}
+
+#[d
