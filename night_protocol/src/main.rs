3fffbf596f2b013c5199422085be322b9995dbca
KE5HBB <114964721+KE5HBB@users.noreply.github.com>
2022-12-20 18:18:59 +0000

    Setting version to 0.2.1-SNAPSHOT

--- night_protocol/src/main.rs
+++ night_protocol/src/main.rs
@@ -27,4 +27,5 @@ impl Fairing for CORS {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-    
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::n
